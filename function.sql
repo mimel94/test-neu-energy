f21b0f46cda54e31ffc3f3b136a1405eff101fac
Miller Garcia <miller.garcia@csiete.org>
2023-11-02 22:08:38 -0500

    refactor function to receive id services

--- function.sql
+++ function.sql
@@ -1,52 +1,78 @@
-CREATE OR REPLACE FUNCTION calculate_energy_bill(OUT EA FLOAT, OUT EE1 FLOAT, OUT EE2 FLOAT, OUT EC FLOAT) AS $$
+CREATE OR REPLACE FUNCTION calculate_energy_bill(
+    IN service_id INT,
+    OUT EA FLOAT,
+    OUT EE1 FLOAT,
+    OUT EE2 FLOAT,
+    OUT EC FLOAT
+) AS $$
 DECLARE
     total_consumption FLOAT;
     total_injection FLOAT;
     cu_tariffs FLOAT;
     c_tariffs FLOAT;
     EE2_tariff FLOAT;
+	voltage_level_var INT;
 BEGIN
-    SELECT SUM(value) INTO total_consumption
+    SELECT voltage_level INTO voltage_level_var FROM services WHERE id_service = service_id;
+
+	SELECT SUM(c.value)
+    INTO total_consumption
     FROM consumption c
     JOIN records r ON c.id_record = r.id_record
-    JOIN services s ON r.id_service = s.id_service
-    WHERE s.voltage_level IN (2, 3);
+    WHERE r.id_service = service_id;
 
-    SELECT SUM(value) INTO total_injection
+    SELECT SUM(i.value)
+    INTO total_injection
     FROM injection i
     JOIN records r ON i.id_record = r.id_record
-    JOIN services s ON r.id_service = s.id_service
-    WHERE s.voltage_level IN (2, 3);
+    WHERE r.id_service = service_id;
 
-    SELECT CU INTO cu_tariffs FROM tariffs WHERE voltage_level IN (2, 3);
-    SELECT C INTO c_tariffs FROM tariffs WHERE voltage_level IN (2, 3);
-
-    EA := total_consumption * cu_tariffs;
-
-    EC := total_injection * c_tariffs;
+	IF voltage_level_var = 2 OR voltage_level_var = 3 THEN
+		SELECT CU INTO cu_tariffs
+		FROM tariffs
+		WHERE id_market = (SELECT id_market FROM services WHERE id_service = service_id)
+		AND voltage_level IN (2, 3);
+	ELSE
+		SELECT CU INTO cu_tariffs
+        FROM tariffs
+        WHERE id_market = (SELECT id_market FROM services WHERE id_service = service_id)
+        AND cdi = (SELECT cdi FROM services WHERE id_service = service_id)
+        AND voltage_level = voltage_level_var;
+	END IF;
+	EA := total_consumption * cu_tariffs;
 
     IF total_injection <= total_consumption THEN
+        -- Tarifa CU negativa
         EE1 := total_injection * (-cu_tariffs);
-        EE2 := 0;
-    ELSE
+	END IF;
+    IF total_injection > total_consumption THEN
+        -- Tarifa CU negativa
         EE1 := total_consumption * (-cu_tariffs);
-        IF total_injection > total_consumption THEN
-            SELECT SUM((i.value - total_consumption) * b.value)
-            INTO EE2_tariff
-            FROM injection i
-            JOIN xm_data_hourly_per_agent b ON i.record_timestamp = b.record_timestamp
-            JOIN records r ON i.id_record = r.id_record
-            JOIN services s ON r.id_service = s.id_service
-            WHERE i.value > total_consumption AND s.voltage_level IN (2, 3);
-        ELSE
-            EE2_tariff := 0;
-        END IF;
+    END IF;
+
+	IF total_injection <= total_consumption THEN
+        EE2 := 0; -- EE2 es igual a cero si la inyección es menor o igual al consumo
+    ELSE
+		SELECT SUM((c.value - i.value) * b.value)
+		INTO EE2_tariff
+		FROM consumption c
+		JOIN injection i ON c.id_record = i.id_record
+		JOIN records r ON c.id_record = r.id_record
+		JOIN xm_data_hourly_per_agent b ON r.record_timestamp = b.record_timestamp
+		WHERE r.id_service = service_id;
+
         EE2 := EE2_tariff;
     END IF;
+
+ 	SELECT C INTO c_tariffs
+    FROM tariffs
+    WHERE id_market = (SELECT id_market FROM services WHERE id_service = service_id)
+    AND voltage_level = voltage_level_var;
+	RAISE NOTICE 'total_injection: %, c_tariffs: %', total_injection, c_tariffs;
+	EC := total_injection * c_tariffs;
 
     RETURN;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM calculate_energy_bill();
-
+SELECT s.id_service, (calculate_energy_bill(s.id_service)).* FROM services s ORDER BY s.id_service;
